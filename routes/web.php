806f33dfd23275586ce7a4693ec866e472c1b147
Alialbolad <alialbolad@medieinstitutet.com>
2020-10-20 22:35:08 +0200

    Vår första Commit

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+  return view('welcome');
+});
+
+// pizza routes
+#pizza view using PizzaController
+Route::get('/pizzas', [PizzaController::class, 'index']);
+Route::get('/pizzas', [PizzaController::class, 'create']);
+Route::post('/pizzas', [PizzaController:: class,'store']);
+Route::get('/pizzas/{id}', [PizzaController::class,'show']);
+Route::delete('/pizzas/{id}', [PizzaController::class,'destroy']);
