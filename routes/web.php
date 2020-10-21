2f199d277f017bcf77c54c9a1e428eddcadcab68
Alialbolad <alialbolad@medieinstitutet.com>
2020-10-21 18:03:43 +0200

    I change the sass file and install.

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+use App\Http\Controllers\PizzaController;
 Route::get('/', function () {
   return view('welcome');
 });
@@ -18,7 +18,7 @@ Route::get('/', function () {
 // pizza routes
 #pizza view using PizzaController
 Route::get('/pizzas', [PizzaController::class, 'index']);
-Route::get('/pizzas', [PizzaController::class, 'create']);
+Route::get('/pizzas/create', [PizzaController::class, 'create']);
 Route::post('/pizzas', [PizzaController:: class,'store']);
 Route::get('/pizzas/{id}', [PizzaController::class,'show']);
 Route::delete('/pizzas/{id}', [PizzaController::class,'destroy']);
